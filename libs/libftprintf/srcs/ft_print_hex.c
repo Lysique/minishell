f8d8270683847896782321c97f6c3b08771a85a7
Slathouwers <sander.lathouwers@live.be>
2021-11-06 09:16:15 +0000

    removed all refercens to 'bonus' targets in libftprintf

--- libs/libftprintf/srcs/ft_print_hex.c
+++ libs/libftprintf/srcs/ft_print_hex.c
@@ -1,37 +1,88 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_hex.c                                     :+:      :+:    :+:   */
+/*   ft_print_hex_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/20 10:18:15 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/29 10:57:52 by slathouw         ###   ########.fr       */
+/*   Created: 2021/09/21 10:09:05 by slathouw          #+#    #+#             */
+/*   Updated: 2021/09/29 12:08:26 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "ft_printf_bonus.h"
 
-void	ft_print_hex(t_format *fmt, va_list ap)
+static void
+	print_prefix_precision(t_field *fld, t_format *fmt, int len, char *str)
 {
-	char			*str;
-	unsigned long	ul;
-	size_t			print_len;
+	if (*fld->parse_ptr == 'p')
+		fmt->num_printed += ft_putstrl_fd("0x", 2, 1);
+	else if (*str != '0')
+	{
+		if (fld->alt_form && *fld->parse_ptr == 'x')
+			fmt->num_printed += ft_putstrl_fd("0x", 2, 1);
+		else if (fld->alt_form && *fld->parse_ptr == 'X')
+			fmt->num_printed += ft_putstrl_fd("0X", 2, 1);
+	}
+	while (len-- > 0)
+		fmt->num_printed += ft_putstrl_fd("0", 1, 1);
+}
 
-	if (*fmt->fstr == 'x' || *fmt->fstr == 'X')
+static void
+	print_width(t_field *fld, t_format *fmt, char *str, int print_len)
+{
+	char	*padding;
+	int		width_remainder;
+
+	padding = " ";
+	if (fld->zero_padding && fld->precision < 0 && !fld->left_align
+		&& !fld->alt_form)
+		padding = "0";
+	width_remainder = fld->min_width;
+	if (*fld->parse_ptr == 'p' || (*str != '0' && fld->alt_form))
+		width_remainder -= 2;
+	if (!(*str == '0' && fld->precision == 0))
+		width_remainder -= ft_max(fld->precision, print_len);
+	while (width_remainder-- > 0)
+		fmt->num_printed += ft_putstrl_fd(padding, 1, 1);
+}
+
+static void
+	print_h(t_field *fld, t_format *fmt, char *str)
+{
+	size_t	print_len;
+	char	*hexstr;
+
+	hexstr = str;
+	print_len = ft_strlen(hexstr);
+	if (!fld->left_align)
+		print_width(fld, fmt, hexstr, print_len);
+	print_prefix_precision(fld, fmt, (fld->precision - print_len), hexstr);
+	if (!(*hexstr == '0' && fld->precision == 0))
+	{
+		fmt->num_printed += ft_putstrl_fd(hexstr, print_len, 1);
+	}
+	if (fld->left_align)
+		print_width(fld, fmt, hexstr, print_len);
+}
+
+void
+	ft_print_hex(t_field *fld, t_format *fmt, va_list ap)
+{
+	char				*str;
+	unsigned long int	ul;
+
+	if (*fld->parse_ptr == 'x' || *fld->parse_ptr == 'X')
 		ul = va_arg(ap, unsigned int);
 	else
 		ul = va_arg(ap, unsigned long int);
-	if (*fmt->fstr == 'x' || *fmt->fstr == 'p' )
+	if (*fld->parse_ptr == 'x' || *fld->parse_ptr == 'p' )
 		str = ft_ultoa_base(ul, "0123456789abcdef");
 	else
 		str = ft_ultoa_base(ul, "0123456789ABCDEF");
 	if (!str)
 		return ;
-	print_len = ft_ultobase_len(ul, "0123456789ABCDEF");
-	if (*fmt->fstr == 'p')
-		fmt->num_printed += ft_putstrl_fd("0x", 2, 1);
-	fmt->num_printed += ft_putstrl_fd(str, print_len, 1);
-	fmt->fstr++;
+	print_h(fld, fmt, str);
+	fmt->fstr = fld->parse_ptr + 1;
 	free(str);
 }
--- libs/libftprintf/srcs/ft_print_str.c
+++ libs/libftprintf/srcs/ft_print_str.c
@@ -1,25 +1,54 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_str.c                                     :+:      :+:    :+:   */
+/*   ft_print_str_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 10:30:19 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/20 10:30:42 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/22 12:41:35 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "ft_printf_bonus.h"
 
-void	ft_print_str(t_format *fmt, va_list ap)
+static void	print_padding(size_t len)
+{
+	while (len--)
+		ft_putchar_fd(' ', 1);
+}
+
+static void	print_s(t_field *fld, t_format *fmt, char *str)
+{
+	size_t	padding;
+
+	padding = 0;
+	if ((size_t)fld->min_width > ft_strlen(str))
+		padding = fld->min_width - ft_strlen(str);
+	if (fld->left_align)
+		fmt->num_printed += ft_putstrl_fd(str, ft_strlen(str), 1);
+	print_padding(padding);
+	fmt->num_printed += padding;
+	if (!fld->left_align)
+		fmt->num_printed += ft_putstrl_fd(str, ft_strlen(str), 1);
+}
+
+void	ft_print_str(t_field *fld, t_format *fmt, va_list ap)
 {
 	char	*str;
+	char	*trunc;
+	int		len;
 
 	str = va_arg(ap, char *);
+	if (fld->precision > -1)
+		len = fld->precision;
+	else
+		len = ft_strlen(str);
+	trunc = ft_strldup(str, len);
 	if (!str)
 		fmt->num_printed += ft_putstrl_fd("(null)", 6, 1);
 	else
-		fmt->num_printed += ft_putstrl_fd(str, ft_strlen(str), 1);
-	fmt->fstr++;
+		print_s(fld, fmt, trunc);
+	free(trunc);
+	fmt->fstr = fld->parse_ptr + 1;
 }
