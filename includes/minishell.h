327b57140fde375666a0cec48a680e0bc9d2c10c
slathouw <sander.lathouwers@live.be>
2021-11-09 15:28:48 +0100

    updated headerfile

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 10:03:16 by slathouw          #+#    #+#             */
-/*   Updated: 2021/11/06 10:14:17 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/11/09 15:28:19 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,24 @@
 # include "../libs/libftprintf/includes/ft_printf_bonus.h"
 
 /*STRUCTS*/
+
+typedef	t_list t_args;
+
+typedef struct s_cmds
+{
+	char	*cmd;
+	t_args	*args;
+	int		pipein;
+	int		pipeout;
+} 				t_cmds;
+
+typedef struct s_cmdline
+{
+	t_cmds *cmds;
+	int	outfile;          
+	int infile;
+	int errfile;
+}			t_cmdline;
 
 /*FUNCTIONS*/
 	/*LEXER*/
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,13 +6,13 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 09:59:19 by slathouw          #+#    #+#             */
-/*   Updated: 2021/11/06 10:34:29 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/11/09 15:26:19 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **env)
 {
 	(void) argc;
 	(void) argv;
