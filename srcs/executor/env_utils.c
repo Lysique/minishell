71d37bf17998b6181689cb1d442f482743ccf25e
Slathouwers <sander.lathouwers@live.be>
2021-12-24 09:33:49 +0000

    cleanup

--- srcs/executor/env_utils.c
+++ srcs/executor/env_utils.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 10:56:36 by slathouw          #+#    #+#             */
-/*   Updated: 2021/12/24 09:14:51 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/12/24 09:33:07 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	env_init(char **envp, t_cmdline *cmdline)
 {
 	char	*line;
-	
+
 	ft_array_init(&cmdline->env_arr, sizeof(char *));
 	while (*envp)
 	{
@@ -26,18 +26,3 @@ void	env_init(char **envp, t_cmdline *cmdline)
 	ft_array_add(&cmdline->env_arr, 0);
 	cmdline->env = cmdline->env_arr.data;
 }
-
-/* 	char	**new_env;
-	char	**temp;
-	size_t	var_count;
-	size_t	i;
-
-	var_count = 0;
-	temp = envp;
-	while (*temp++)
-		var_count++;
-	new_env = ft_calloc(var_count + 1, sizeof(char *));
-	i = -1;
-	while (++i < var_count)
-		new_env[i] = ft_strdup(envp[i]);
-	return (new_env); */
