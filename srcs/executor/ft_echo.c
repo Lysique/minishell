254edbf7e6ae81b54f369e3e7cec8b9525e02c0d
Slathouwers <sander.lathouwers@live.be>
2021-12-24 09:30:32 +0000

    added todo for ft_echo

--- srcs/executor/ft_echo.c
+++ srcs/executor/ft_echo.c
@@ -6,12 +6,13 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 11:03:42 by tamighi           #+#    #+#             */
-/*   Updated: 2021/12/23 10:22:37 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/12/24 08:10:00 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+/*TODO: echo should not use printf but instead ft_putstr_fd with fd = pipeout?*/
 int	ft_echo(t_cmdline *cmdline)
 {
 	t_args	*tmp;
