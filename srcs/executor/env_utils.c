9cc6094263650c638d548c39b62662b3e91e787a
slathouw <sander.lathouwers@live.be>
2021-12-27 13:26:33 +0100

    TODO's for env_utils, to be used in ft_export

--- srcs/executor/env_utils.c
+++ srcs/executor/env_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
+/*   By: slathouw <slathouw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 10:56:36 by slathouw          #+#    #+#             */
-/*   Updated: 2021/12/24 09:33:07 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/12/27 13:26:01 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,50 @@ void	env_init(char **envp, t_cmdline *cmdline)
 	}
 	ft_array_add(&cmdline->env_arr, 0);
 	cmdline->env = cmdline->env_arr.data;
+}
+
+
+int is_nameequword_format(char *line)
+{
+	char **split;
+
+	if (!ft_strchr(line, '='))
+		return (0);
+	split = ft_split(line, '=');
+	if
+}
+
+int is_valid_identifier(char *name)
+{
+	//if start is NOT numerical and name is NOT number
+}
+
+int	env_add_var(t_cmdline *cmdline, char *line)
+{
+	//TODO: check format = NAME=word
+	//TODO: check NAME not exist
+	//TODO: add "NAME=word" to env array
+}
+
+int env_set_var(t_cmdline *cmdline, char *line)
+{
+	//TODO: check format = "NAME=word"
+	//TODO: check NAME exists
+		//IF exists: set line to new line
+		//IF not exists: add line to envp
+}
+
+char *get_var_value(t_cmdline *cmdline, char *var_name)
+{
+	//TODO: check if NAME in envp, if it is, return value
+}
+
+int env_unset(t_cmdline *cmdline, char *var_name)
+{
+	
+}
+
+int sort_env(t_cmdline *cmdline)
+{
+	
 }
