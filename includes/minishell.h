fbfd23cfccedd88463f89dfbe9e4b5bbb1d5c401
slathouw <sander.lathouwers@live.be>
2021-12-30 13:24:06 +0100

    compilation fixes

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
+/*   By: slathouw <slathouw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 10:03:16 by slathouw          #+#    #+#             */
-/*   Updated: 2021/12/30 10:25:04 by tamighi          ###   ########.fr       */
+/*   Updated: 2021/12/30 11:40:08 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,7 @@ void		prompt(t_cmdline *cmdline);
 t_cmdline	*cl_ptr(t_cmdline *cl);
 
 void		execute_minishell(char **env);
-void		signal_management();
+void		signal_management(void);
 void		*ft_malloc(int size, void *p);
 char		*check_cmdline(char **arr);
 
