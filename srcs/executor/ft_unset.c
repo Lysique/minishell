346793f15102d3843533163378026b70640f21ca
slathouw <sander.lathouwers@live.be>
2022-01-04 14:48:40 +0100

    fixed exit status

--- srcs/executor/ft_unset.c
+++ srcs/executor/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 15:06:38 by slathouw          #+#    #+#             */
-/*   Updated: 2021/12/28 15:43:03 by slathouw         ###   ########.fr       */
+/*   Updated: 2022/01/04 14:42:37 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,19 @@ int	ft_unset(t_cmdline *cmdline)
 	args = cmd.args;
 	fail = 0;
 	if (!args)
-	{
-		ft_putstr_fd("minishell: export: '", 2);
-		ft_putstr_fd("': not enough arguments\n", 2);
-		fail = 1;
-		return (!fail);
-	}
+		return (EXIT_SUCCESS);
 	while (args)
 	{
 		argline = (char *) args->content;
 		if (is_nameequword_format(argline) != 2)
 		{
-			ft_putstr_fd("minishell: export: '", 2);
-			ft_putstr_fd(argline, 2);
-			ft_putstr_fd("': not a valid identifier\n", 2);
+			ft_fdprintf(2, "minishell: export: '%s': not a valid identifier\n",
+				argline);
 			fail = 1;
 			continue ;
 		}
 		env_unset(cmdline, argline);
 		args = args->next;
 	}
-	return (!fail);
+	return (fail);
 }
