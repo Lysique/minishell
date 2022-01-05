5d3265ddead961ff83b7e3f00b8e17b4f8d55b08
slathouw <sander.lathouwers@live.be>
2022-01-05 16:58:05 +0100

    fixed no-arg export

--- srcs/executor/ft_export.c
+++ srcs/executor/ft_export.c
@@ -6,11 +6,84 @@
 /*   By: slathouw <slathouw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/24 06:57:35 by slathouw          #+#    #+#             */
-/*   Updated: 2022/01/05 13:01:36 by slathouw         ###   ########.fr       */
+/*   Updated: 2022/01/05 16:57:45 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+static char	**get_dup_env(t_cmdline *cmdline)
+{
+	char	**dup;
+	int		count;
+
+	count = 0;
+	while (cmdline->env[count])
+		count++;
+	dup = (char **) malloc(sizeof(char *) * (count + 1));
+	dup[count] = NULL;
+	count--;
+	while (count > -1)
+	{
+		dup[count] = cmdline->env[count];
+		count--;
+	}
+	return (dup);
+}
+
+static void	ft_swap(char *a, char *b)
+{
+	char	*tmp;
+
+	tmp = a;
+	a = b;
+	b = a;
+}
+
+static void	print_export(char **env)
+{
+	int	i;
+	int	j;
+	int	flagequ;
+
+	i = -1;
+	while (env[++i])
+	{
+		flagequ = 1;
+		j = -1;
+		ft_putstr_fd("declare -x ", 1);
+		while (env[i][++j])
+		{
+			ft_putchar_fd(env[i][j], 1);
+			if (env[i][j] == '=' && flagequ && flagequ--)
+				ft_putchar_fd('"', 1);
+		}
+		if (!flagequ)
+			ft_putchar_fd('"', 1);
+		ft_putchar_fd('\n', 1);
+	}
+}
+
+static void	env_sort(t_cmdline *cmdline)
+{
+	char	**dup;
+	int		i;
+	int		j;
+
+	dup = get_dup_env(cmdline);
+	i = -1;
+	while (dup[++i])
+	{
+		j = i;
+		while (dup[++j])
+		{
+			if (ft_strequ(dup[i], dup[j]) > 0)
+				ft_swap(dup[i], dup[j]);
+		}
+	}
+	print_export(dup);
+	free(dup);
+}
 
 int	ft_export(t_cmdline *cmdline)
 {
@@ -22,7 +95,7 @@ int	ft_export(t_cmdline *cmdline)
 	args = cmd.args;
 	fail = 0;
 	if (!args)
-		ft_env(cmdline);
+		env_sort(cmdline);
 	while (args)
 	{
 		argline = (char *) args->content;
