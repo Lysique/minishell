f5d7930c50e5a9dffa5a2387c44d9049b44de57a
slathouw <sander.lathouwers@live.be>
2022-01-05 13:00:55 +0100

    fixed execve using split of command instead of the expanded cmd and t_args

--- srcs/executor/redir_exec.c
+++ srcs/executor/redir_exec.c
@@ -6,11 +6,32 @@
 /*   By: slathouw <slathouw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/26 11:11:29 by tamighi           #+#    #+#             */
-/*   Updated: 2022/01/05 12:30:30 by slathouw         ###   ########.fr       */
+/*   Updated: 2022/01/05 12:59:01 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+char	**cmdline_to_arr(t_cmdline *cl)
+{
+	char	**arr;
+	t_args	*currarg;
+	int		count;
+	int		i;
+
+	arr = NULL;
+	count = 1 + ft_lstsize(cl->cmds->args) + 1;
+	arr = ft_malloc(sizeof(char *) * count, NULL);
+	i = 0;
+	arr[i] = cl->cmds->cmd;
+	currarg = cl->cmds->args;
+	while (++i < count - 1)
+	{
+		arr[i] = (char *)currarg->content;
+		currarg = currarg->next;
+	}
+	return (arr);
+}
 
 void	redir_exec(t_cmdline *cmdline)
 {
@@ -18,7 +39,7 @@ void	redir_exec(t_cmdline *cmdline)
 	char	*path;
 
 	dup2(cmdline->cmds->fd_in, 0);
-	act = ms_split(cmdline->cmds->command, ' ');
+	act = cmdline_to_arr(cmdline);
 	path = find_path(act[0], cmdline->env);
 	if (!path)
 	{
@@ -26,4 +47,5 @@ void	redir_exec(t_cmdline *cmdline)
 		exit(EXIT_CMD_NOT_FOUND_ERR);
 	}
 	execve(path, act, cmdline->env);
+	ft_malloc(-1, act);
 }
