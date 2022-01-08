8d892b1c1bd2f19a665a101cf8ceeee130394757
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-01-08 09:47:04 +0100

    parentheses correctly taken into account

--- srcs/executor/check_exit_status.c
+++ srcs/executor/check_exit_status.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 12:46:07 by tamighi           #+#    #+#             */
-/*   Updated: 2022/01/08 09:36:42 by tamighi          ###   ########.fr       */
+/*   Updated: 2022/01/08 09:45:53 by tamighi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ void	check_exit_status(t_cmdline *cmdline)
 	int		pipe;
 
 	cmds = cmdline->cmds;
-	parentheses = cmds->parentheses;
 	cmdline->exit = WEXITSTATUS(cmds->exitstatus);
 	if ((cmds->pipetype == 3 && cmdline->exit)
 		|| (cmds->pipetype == 2 && cmdline->exit == 0))
 	{
 		pipe = cmds->pipetype;
 		cmdline->cmds++;
+		parentheses = cmdline->cmds->parentheses;
 		while (parentheses || cmdline->cmds->pipetype == pipe)
 		{
-			parentheses += cmdline->cmds->parentheses;
 			cmdline->cmds++;
+			parentheses += cmdline->cmds->parentheses;
 		}
 	}
 }
