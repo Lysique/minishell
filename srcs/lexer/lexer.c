4aaf13a09f8b65b94de106890aff0df177ab2044
Slathouwers <sander.lathouwers@live.be>
2022-01-21 08:36:14 +0000

    initialised allocated mem to zero

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 09:04:02 by tamighi           #+#    #+#             */
-/*   Updated: 2022/01/18 09:28:24 by slathouw         ###   ########.fr       */
+/*   Updated: 2022/01/21 08:00:50 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ static char	**create_token_arr(char *line)
 
 	tokens = nb_tokens(line);
 	arr = ft_malloc(sizeof(char *) * (tokens + 1), 0);
+	ft_bzero(arr, sizeof(char *) * (tokens + 1));
 	arr = tokens_in_arr(line, arr);
 	return (arr);
 }
